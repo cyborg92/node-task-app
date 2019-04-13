88e6582c2049e92d293fd2cf67b179b402f7e2f5
cyborg92 <rachithanda93@yahoo.com>
2019-04-13 08:50:32 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+const jwt = require('jsonwebtoken');
+
+const myFunction = async () => {
+    const token=jwt.sign({_id:'abc123'},'thisisrachithanda',{expiresIn:'1 second'});
+    console.log(token);
+
+    const data=jwt.verify(token,'thisisrachithanda');
+    console.log('data',data);
+}
+
+// myFunction()
---
+++ src/models/user.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
+})
+// hash the plaintext password
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+//auth token generation method for each user object
+
+userSchema.methods.generateAuthToken= async function () {
+    const user = this;
+    const token=jwt.sign({_id:user._id.toString()},'thisisrachithanda');
+    user.tokens=user.tokens.concat({token});
+    await user.save();
+    return token;
+}
+
+// hide private data
+
+userSchema.methods.toJSON=async function(){
+    const user=this;
+    userObject=user.toObject();
+    delete userObject.password;
+    delete userObject.tokens;
+
+    return userObject;
+}
+// custom method for schema
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+    return user;
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
